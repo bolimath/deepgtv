8a8f8d443f012c1c511222e833eb970a7398d01f
Huy D. Vu <huyvd7@gmail.com>
2020-04-04 20:59:11 -0400

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -27,8 +27,7 @@ def denoise(inp, gtv, argref, normalize=False, stride=36, width=324, prefix='_',
     except Exception:
         from skimage.measure import compare_ssim
     if filetype=='npy':
-        sample = np.load(inp)
-        print(sample.min(), sample.max())
+        sample = np.load(inp)*255
     else:
         sample = cv2.imread(inp)
     if width==None:
