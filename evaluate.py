5f46b675b3a205b5bb69a5d5a3b68f8dd0d5fd64
Huy D. Vu <huyvd7@gmail.com>
2020-04-04 20:46:50 -0400

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -139,6 +139,7 @@ def denoise(inp, gtv, argref, normalize=False, stride=36, width=324, prefix='_',
         opath = "./{0}_{1}".format(prefix, filename)
         opath = opath[:-3] + "png"
     if argref:
+        print(d.min(), d.max(), tref.min(), tref.max())
         psnr2 = cv2.PSNR((tref/255.0).astype(d.dtype), d)
         mse = ((d-(tref/255.0))**2).mean()*255
         print("MSE: {:.6f}".format(mse))
